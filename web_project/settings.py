be8cff0f3811c46fe9ffc29b9d1c02868bdc9ce9
adan <adanzoabi20797@gmail.com>
2022-05-30 15:56:53 +0300

    first commit

--- web_project/settings.py
+++ web_project/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY =config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =config('DEBUG',default=True,cast=bool)
 
-ALLOWED_HOSTS = ['project-env.eba-sygbqyvs.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
